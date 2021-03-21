8b5b0987c79f5a8bf4b0a00be7594ab8a514d460
Ethan Padden <ethanpadden@gmail.com>
2021-03-21 16:57:23 +0000

    Refactor: get conversations handler to cleaner code

--- src/main/java/com/perrys/RequestHandlers/GetConversationHandler.java
+++ src/main/java/com/perrys/RequestHandlers/GetConversationHandler.java
@@ -18,56 +18,65 @@ import java.util.List;
 import java.util.Map;
 
 public class GetConversationHandler implements RequestHandler<ConversationRequest, GatewayResponse> {
-    private DynamoDB dynamoDB;
     private AmazonDynamoDBClient client;
-    private String DYNAMODB_TABLE_NAME = "Messages";
+    private String DB_MESSAGES_TABLE_NAME = "Messages";
     private Regions REGION = Regions.EU_WEST_1;
 
     @Override
     public GatewayResponse handleRequest(ConversationRequest conversationRequest, Context context)
     {
-        this.initDynamoDbClient();
-        Map<String, AttributeValue> expressionAttributeValues =
-                new HashMap<String, AttributeValue>();
-        expressionAttributeValues.put(":id1", new AttributeValue().withS(conversationRequest.getUserId1()));
-        expressionAttributeValues.put(":id2", new AttributeValue().withS(conversationRequest.getUserId2()));
+        // Create response object
+        GatewayResponse response;
 
-        String filterExpression =
-                "senderUserId = :id1"
-                        + " OR senderUserId = :id2"
-                        + " OR recipientUserId = :id1"
-                        + " OR recipientUserId = :id2";
+        try {
+            // Initialise DynamoDB client
+            client = new AmazonDynamoDBClient();
+            client.setRegion(Region.getRegion(REGION));
 
-        ScanRequest scanRequest = new ScanRequest()
-                .withTableName("Messages")
-                .withFilterExpression(filterExpression)
-                .withExpressionAttributeValues(expressionAttributeValues);
-        ScanResult scanResult =  client.scan(scanRequest);
-        List<Map<String, AttributeValue>> items = scanResult.getItems();
-        JsonObject jsonMessageList = new JsonObject();
-        for (int i = 0; i < items.size(); i++) {
-            String messageId = items.get(i).get("messageId").toString();
-            String timestamp = items.get(i).get("timestamp").toString();
-            String body = items.get(i).get("body").toString();
-            String recipientUserId = items.get(i).get("recipientUserId").toString();
-            String senderUserId = items.get(i).get("senderUserId").toString();
+            // Cannot use literals in a filter expression
+            // Use hashmap to map them
+            Map<String, AttributeValue> expressionAttributeValues =
+                    new HashMap<String, AttributeValue>();
+            expressionAttributeValues.put(":id1", new AttributeValue().withS(conversationRequest.getUserId1()));
+            expressionAttributeValues.put(":id2", new AttributeValue().withS(conversationRequest.getUserId2()));
 
-            JsonObject jsonMessage = new JsonObject();
-            jsonMessage.addProperty("messageId", messageId);
-            jsonMessage.addProperty("timestamp", timestamp);
-            jsonMessage.addProperty("body", body);
-            jsonMessage.addProperty("recipientUserId", recipientUserId);
-            jsonMessage.addProperty("senderUserId", senderUserId);
-            jsonMessageList.add(Integer.toString(i), jsonMessage);
+            // Build filter expression
+            String filterExpression = "senderUserId = :id1"
+                    + " OR senderUserId = :id2"
+                    + " OR recipientUserId = :id1"
+                    + " OR recipientUserId = :id2";
+
+            // Execute scan request and retrieve a list of DB items
+            ScanRequest scanRequest = new ScanRequest()
+                    .withTableName(DB_MESSAGES_TABLE_NAME)
+                    .withFilterExpression(filterExpression)
+                    .withExpressionAttributeValues(expressionAttributeValues);
+            ScanResult scanResult = client.scan(scanRequest);
+            List<Map<String, AttributeValue>> items = scanResult.getItems();
+
+            // Create JSON object for response body
+            JsonObject jsonMessageList = new JsonObject();
+            for (int i = 0; i < items.size(); i++) {
+                String messageId = items.get(i).get("messageId").toString();
+                String timestamp = items.get(i).get("timestamp").toString();
+                String body = items.get(i).get("body").toString();
+                String recipientUserId = items.get(i).get("recipientUserId").toString();
+                String senderUserId = items.get(i).get("senderUserId").toString();
+
+                JsonObject jsonMessage = new JsonObject();
+                jsonMessage.addProperty("messageId", messageId);
+                jsonMessage.addProperty("timestamp", timestamp);
+                jsonMessage.addProperty("body", body);
+                jsonMessage.addProperty("recipientUserId", recipientUserId);
+                jsonMessage.addProperty("senderUserId", senderUserId);
+                jsonMessageList.add(Integer.toString(i), jsonMessage);
+            }
+
+            response = new GatewayResponse(jsonMessageList.toString(), 200);
+        } catch (Exception e) {
+            response = new GatewayResponse("There was an error accessing the database", 500);
         }
-        GatewayResponse response = new GatewayResponse(jsonMessageList.toString(), 200);
-        return response;
-    }
 
-    private void initDynamoDbClient()
-    {
-        client = new AmazonDynamoDBClient();
-        client.setRegion(Region.getRegion(REGION));
-        this.dynamoDB = new DynamoDB(client);
+        return response;
     }
 }
