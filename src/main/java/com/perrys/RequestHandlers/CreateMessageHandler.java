4fb35213c840a7741fecd1c34046cc901b2afc0e
Ethan Padden <ethanpadden@gmail.com>
2021-03-21 16:38:53 +0000

    Refactor: Create message handler to cleaner code

--- src/main/java/com/perrys/RequestHandlers/CreateMessageHandler.java
+++ src/main/java/com/perrys/RequestHandlers/CreateMessageHandler.java
@@ -24,47 +24,44 @@ public class CreateMessageHandler implements RequestHandler<Message, GatewayResp
     @Override
     public GatewayResponse handleRequest(Message message, Context context)
     {
-        this.initDynamoDbClient();
-        persistData(message);
-        GatewayResponse personResponse = new GatewayResponse("Message created", 200);
-        return personResponse;
-    }
+        // Create response object
+        GatewayResponse response;
 
-    private PutItemOutcome persistData(Message messageRequest)
-            throws ConditionalCheckFailedException
-    {
-        // Get the table from the DB object
-        Table table = dynamoDB.getTable(DYNAMODB_TABLE_NAME);
-
-        // Generate a random UUID as a string
-        String uuid = UUID.randomUUID().toString(); // TODO: move to empty constructor and set as member variable?
-
-        // Get current timestamp
-        // TODO: move to empty constructor and set as member variable?
-        Timestamp timestampObj = new Timestamp(System.currentTimeMillis());
-//        String timestampStr = Long.toString(timestampObj.getTime());
-        long timestamp = timestampObj.getTime();
         try {
-            System.out.println("Creating message in database...");
-            PutItemOutcome outcome = table
-                    .putItem(new Item().withPrimaryKey("messageId", uuid)
-                            .withString("senderUserId", messageRequest.getSenderUserId())
-                            .withString("recipientUserId", messageRequest.getRecipientUserId())
-                            .withNumber("timestamp", timestamp)
-                            .withString("body", messageRequest.getBody()));
-            System.out.println("PutItem succeeded:\n" + outcome.getPutItemResult());
-            return outcome;
+            // Initialise DynamoDB client
+            AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+            client.setRegion(Region.getRegion(REGION));
+            this.dynamoDB = new DynamoDB(client);
+
+            // Get the table from the DB object
+            Table table = dynamoDB.getTable(DYNAMODB_TABLE_NAME);
+
+            // Generate a random UUID as a string
+            String uuid = UUID.randomUUID().toString();
+
+            // Get current timestamp
+            // TODO: move to empty constructor and set as member variable?
+            Timestamp timestampObj = new Timestamp(System.currentTimeMillis());
+            long timestamp = timestampObj.getTime();
+
+            Item newMessageDBItem = new Item()
+                    .withPrimaryKey("messageId", uuid)
+                    .withString("senderUserId", message.getSenderUserId())
+                    .withString("recipientUserId", message.getRecipientUserId())
+                    .withNumber("timestamp", timestamp)
+                    .withString("body", message.getBody());
+
+            // Put item into table
+            table.putItem(newMessageDBItem);
+
+            response = new GatewayResponse("Message created", 200);
+
         } catch (Exception e) {
-            System.err.println("Unable to add message");
-            System.err.println(e.getMessage());
-            return null; // TODO: throw custom exception here and handle response in handleRequest - set status code accordingly
+            response = new GatewayResponse("There was an error accessing the database: ", 500);
         }
+
+        return response;
     }
 
-    private void initDynamoDbClient()
-    {
-        AmazonDynamoDBClient client = new AmazonDynamoDBClient();
-        client.setRegion(Region.getRegion(REGION));
-        this.dynamoDB = new DynamoDB(client);
-    }
+
 }
