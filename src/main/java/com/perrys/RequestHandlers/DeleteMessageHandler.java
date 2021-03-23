c022222310a238fbb24dc4a91864ccbbcac7c41f
Ethan Padden <ethanpadden@gmail.com>
2021-03-23 11:45:40 +0000

    Bug fix: Delete message handler was unable to accept String as an input - wrapped as an UpdateMessageRequest object

--- src/main/java/com/perrys/RequestHandlers/DeleteMessageHandler.java
+++ src/main/java/com/perrys/RequestHandlers/DeleteMessageHandler.java
@@ -7,18 +7,20 @@ import com.amazonaws.services.dynamodbv2.document.*;
 import com.amazonaws.services.dynamodbv2.document.spec.DeleteItemSpec;
 import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
 import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
+import com.amazonaws.services.dynamodbv2.model.AmazonDynamoDBException;
 import com.amazonaws.services.dynamodbv2.model.ReturnValue;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.perrys.GatewayResponse;
+import com.perrys.RequestObjects.UpdateMessageRequest;
 
-public class DeleteMessageHandler implements RequestHandler<String, GatewayResponse> {
+public class DeleteMessageHandler implements RequestHandler<UpdateMessageRequest, GatewayResponse> {
     private DynamoDB dynamoDB;
     private String DYNAMODB_TABLE_NAME = "Messages";
     private Regions REGION = Regions.EU_WEST_1;
 
     @Override
-    public GatewayResponse handleRequest(String messageId, Context context)
+    public GatewayResponse handleRequest(UpdateMessageRequest message, Context context)
     {
         // Create response object
         GatewayResponse response;
@@ -34,7 +36,7 @@ public class DeleteMessageHandler implements RequestHandler<String, GatewayRespo
 
             // Build deleteItemSpec
             DeleteItemSpec deleteItemSpec = new DeleteItemSpec()
-                    .withPrimaryKey(new PrimaryKey("messageId", messageId));
+                    .withPrimaryKey(new PrimaryKey("messageId", message.getMessageId()));
 
             // Delete item from database
             table.deleteItem(deleteItemSpec);
@@ -42,8 +44,14 @@ public class DeleteMessageHandler implements RequestHandler<String, GatewayRespo
             response = new GatewayResponse("Message deleted", 200);
         } catch (IllegalArgumentException e) {
             response = new GatewayResponse("There was an error in the input", 400);
+        }  catch (AmazonDynamoDBException e) {
+            if(e.getMessage().contains("ValidationException") || e.getMessage().contains("invalid value")) {
+                response = new GatewayResponse("There was an error in the input", 400);
+            } else {
+                response = new GatewayResponse("There was an error accessing the database", 500);
+            }
         } catch (Exception e) {
-            response = new GatewayResponse("There was an error accessing the database", 500);
+            response = new GatewayResponse("There was an error accessing the database: " + e.getClass(), 500);
         }
 
         return response;
